02c972c7cf91ac73af09a8e103165cbe44058ec5
Michel <michelouali0@gmail.com>
2026-02-17 17:14:00 +0100

    push rotate and reverse_rotate upgraded with circular lists

--- srcs/instructions.c
+++ srcs/instructions.c
@@ -96,3 +96,56 @@ on dit si cest un bon nombre genre pas trop grand
 controle de doublon
 on l'ajoute a la pile et maj des index*/
 
+
+
+void    push_to_stack(t_stack **src, t_stack **dest)
+{
+        t_stack     *temp;
+        int         lst_size_src;
+        int         lst_size_dest;
+
+        if (!(*src))
+            return ;
+        lst_size_src  = ft_lst_size(src);
+        lst_size_dest = ft_lst_size(dest);
+        temp = *src;
+        if (lst_size_src > 1)
+        {
+            (*src)->prev->next = (*src)-> next;
+            (*src)->next->prev = (*src)->prev;
+            *src = (*src)->next;
+
+        }
+        else if (lst_size_src == 1)
+        {
+            *src = NULL;
+        }
+        if (lst_size_dest == 0)
+        {
+            *dest = temp;
+            (*dest)->next = temp;
+            (*dest)->prev = temp;
+        }
+        else
+        {
+            temp->next = *dest;
+            temp->prev = (*dest)->prev;
+            (*dest)->prev->next = temp;
+            (*dest)->prev = temp;
+            *dest = temp;
+        }
+}
+
+void    rotate(t_stack **src)
+{
+    if (!*src || ((*src)->next == *src))
+        return;
+    *src = (*src)->next;
+}
+
+void reverse_rotate(t_stack **src)
+{
+    if (!*src || (*src)->next == *src)
+        return ;
+    *src = (*src)->prev;
+}
