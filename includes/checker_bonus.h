9409e956727fb290b20003f5daec17707e2d7a25
Michel OUALI <michelouali0@gmail.com>
2026-02-23 21:05:36 +0100

    rangement dans les headers

--- includes/checker_bonus.h
+++ includes/checker_bonus.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   checker_bonus.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miouali <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 17:23:01 by miouali           #+#    #+#             */
-/*   Updated: 2026/02/23 17:23:43 by miouali          ###   ########.fr       */
+/*   Updated: 2026/02/23 21:01:32 by miouali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,39 @@
 
 typedef struct s_stack
 {
-	long            nbr;
-	long            index;
-	struct s_stack  *target_node;
-	struct s_stack  *next;
-	struct s_stack  *prev;
-	int             pos;
-	int             cost_a;
-	int             cost_b;
-	int             above_median;
-	int             cheapest;
+	long			nbr;
+	long			index;
+	struct s_stack	*target_node;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+	int				pos;
+	int				cost_a;
+	int				cost_b;
+	int				above_median;
+	int				cheapest;
 }	t_stack;
 
-void	execute_instruction(char *line, t_stack **stack_a, t_stack **stack_b);
-int		is_an_instruction(char *line);
+//Parsing and validation
+char	**join_and_split(char **av);
+char	*ft_strbigjoin(char **str_tab);
+int		ft_atoll(const char *str, int *error);
 int		parsing(char **av, t_stack **stack_a);
-int		is_sorted(t_stack *stack);
-int		stack_size(t_stack *stack);
+
+//Memory Management
+void	ft_free_stack(t_stack **stack);
+
+//Linked List Management
+void	init_index(t_stack **stack_a);
 void	send_to_stack(t_stack **dest, t_stack *temp);
+int		stack_size(t_stack *stack);
+
+//Core Instructions
 void	push_to_stack(t_stack **src, t_stack **dest);
 void	rotate(t_stack **src);
 void	reverse_rotate(t_stack **src);
 void	swap(t_stack *stack);
-int		checker(char *str);
-char	*ft_strbigjoin(char **str_tab);
-void	ft_free_stack(t_stack **stack);
-int		ft_atoll(const char *str, int *error);
+
+//Checker Rules
 void	ss(t_stack *stack_a, t_stack *stack_b);
 void	sa(t_stack *stack);
 void	sb(t_stack *stack);
@@ -54,9 +61,13 @@ void	rra(t_stack **stack_a);
 void	rrb(t_stack **stack_b);
 void	rr(t_stack **a, t_stack **b);
 void	rrr(t_stack **a, t_stack **b);
-void	init_index(t_stack **stack_a);
-int		fetch_and_execute(char *line, t_stack **stack_a, t_stack **stack_b);
+
+//Instruction Handling
 void	execute_instruction(char *line, t_stack **stack_a, t_stack **stack_b);
 int		is_an_instruction(char *line);
+int		fetch_and_execute(char *line, t_stack **stack_a, t_stack **stack_b);
+
+//Verification
+int		is_sorted(t_stack *stack);
 
 #endif
--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 15:13:27 by miouali           #+#    #+#             */
-/*   Updated: 2026/02/23 12:08:52 by miouali          ###   ########.fr       */
+/*   Updated: 2026/02/23 20:57:41 by miouali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,33 +26,44 @@
 
 typedef struct s_stack
 {
-	long            nbr;
-	long            index;
-	struct s_stack  *target_node;
-	struct s_stack  *next;
-	struct s_stack  *prev;
-	int             pos;
-	int             cost_a;
-	int             cost_b;
-	int             above_median;
-	int             cheapest;
+	long			nbr;
+	long			index;
+	struct s_stack	*target_node;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+	int				pos;
+	int				cost_a;
+	int				cost_b;
+	int				above_median;
+	int				cheapest;
 }	t_stack;
 
-int		check_double(t_stack *stack, int nb);
+//Parsing and validation
 char	**join_and_split(char **av);
+char	*ft_strbigjoin(char **str_tab);
+int		ft_atoll(const char *str, int *error);
+int		check_double(t_stack *stack, int nb);
 int		ft_check_and_add(t_stack **stack_a, long nb, int *error);
 int		parsing(char **av, t_stack **stack_a);
-char	*ft_strbigjoin(char **str_tab);
+
+//Memory Management
 void	ft_free_stack(t_stack **stack);
 void	ft_free_tab(char **tab);
 void	ft_free_all(t_stack **stack, char **tab);
-int		ft_atoll(const char *str, int *error);
-void	ft_display_stacks(t_stack *a, t_stack *b);
+
+//Linked List Management
+t_stack	*ft_lstnew_stack(int nbr);
 void	init_index(t_stack **stack_a);
+void	ft_lstadd_back_stack(t_stack **lst, t_stack *new);
+int		stack_size(t_stack *stack);
+
+//Core Instructions
 void	push_to_stack(t_stack **src, t_stack **dest);
 void	rotate(t_stack **src);
 void	reverse_rotate(t_stack **src);
 void	swap(t_stack *stack);
+
+//Push_swap Rules
 void	sa(t_stack *stack);
 void	sb(t_stack *stack);
 void	pa(t_stack **stack_a, t_stack **stack_b);
@@ -63,33 +74,37 @@ void	rra(t_stack **stack_a);
 void	rrb(t_stack **stack_b);
 void	rr(t_stack **a, t_stack **b);
 void	rrr(t_stack **a, t_stack **b);
-void	sort_three(t_stack **stack);
-void	ft_lstadd_back_stack(t_stack **lst, t_stack *new);
-t_stack	*ft_lstnew_stack(int nbr);
-int		stack_size(t_stack *stack);
-void	set_position(t_stack *stack);
+
+//Algorithm & Calculations
 t_stack	*find_max_stack(t_stack *stack);
+t_stack	*find_min_stack(t_stack *stack);
+t_stack	*get_cheapest(t_stack *stack);
+void	set_position(t_stack *stack);
+void	set_target_b_to_a(t_stack **stack_a, t_stack **stack_b);
 void	set_target_a_to_b(t_stack *stack_a, t_stack *stack_b);
 void	set_cost(t_stack *stack_a, t_stack *stack_b);
+void	set_cheapest(t_stack *stack_a);
+int		find_cost(t_stack *temp_a);
 int		abs(int a);
 int		max(int a, int b);
 int		max_abs(int a, int b);
-void	set_cheapest(t_stack *stack_a);
-void	rotate_both(t_stack **stack_a, t_stack **stack_b,
-			t_stack *cheapest_node);
-void	finish_rotation(t_stack **stack_a, t_stack **stack_b,
-			t_stack *cheapest_node);
-t_stack	*get_cheapest(t_stack *stack);
+
+//Movements & Sorting
+void	sort_three(t_stack **stack);
 void	move_a_to_b(t_stack **stack_a, t_stack **stack_b);
-t_stack	*find_min_stack(t_stack *stack);
-void	set_target_b_to_a(t_stack **stack_a, t_stack **stack_b);
 void	finish_rotation_only_a(t_stack **stack_a, t_stack *target_node);
 void	move_b_to_a(t_stack **stack_a, t_stack **stack_b);
-int		is_sorted(t_stack *stack);
 void	init_nodes_a(t_stack *stack_a, t_stack *stack_b);
 void	init_nodes_b(t_stack *stack_a, t_stack *stack_b);
 void	sort_stacks(t_stack **stack_a, t_stack **stack_b);
 void	min_on_top(t_stack **a);
-int		find_cost(t_stack *temp_a);
+void	rotate_both(t_stack **stack_a, t_stack **stack_b,
+			t_stack *cheapest_node);
+void	finish_rotation(t_stack **stack_a, t_stack **stack_b,
+			t_stack *cheapest_node);
+int		is_sorted(t_stack *stack);
+
+//Debug
+void	ft_display_stacks(t_stack *a, t_stack *b);
 
 #endif
